81a8fa8783f25c05b6fc4ffdd7d49b77b3a15d46
TeoRojas <teofilo.rojas.mata@gmail.com>
2023-01-10 21:43:07 +0100

    Added relative paths

--- mysite/app.py
+++ mysite/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 import random
 from datetime import datetime, timedelta
+from pathlib import Path
+
+THIS_FOLDER = Path(__file__).parent.resolve()
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -21,8 +24,8 @@ def get_current_week():
     """
     -weekday- corresponds to the position of the day of the week in a list,
     Monday = 0, Tuesday = 1 ... Sunday = 6.
-    So to fill the -current_week- list with the number of the corresponding day, 
-    -weekday- is used as the iterator of the position in the list. 
+    So to fill the -current_week- list with the number of the corresponding day,
+    -weekday- is used as the iterator of the position in the list.
     """
 
     today = datetime.today()
@@ -38,13 +41,13 @@ def get_current_week():
         current_week.append(number_day.day)
         iterator += 1
 
-    return current_week   
+    return current_week
 
 
 def get_all_wods():
     wods = []
-    
-    with open('static/bbdd/BBDD_Gymnastics_WODs.csv', 'r') as file:
+
+    with open(THIS_FOLDER / 'static/bbdd/BBDD_Gymnastics_WODs.csv', 'r') as file:
         lines =  file.readlines()
 
         for i in range(1, len(lines)):
@@ -53,13 +56,13 @@ def get_all_wods():
 
             wods.append(Wod(*args))
 
-    return wods  
+    return wods
 
 
 def get_wods_done():
     wods_done = []
 
-    with open('static/bbdd/WODs_done.csv', 'r') as file:
+    with open(THIS_FOLDER / 'static/bbdd/WODs_done.csv', 'r') as file:
         for line in file:
             wods_done.append(int(line))
 
@@ -67,16 +70,16 @@ def get_wods_done():
 
 
 def get_wod_number_not_done(num_wods, wods_done):
-    """obtaining a WOD that has not 
+    """obtaining a WOD that has not
     yet been done (is not in the list of events)"""
 
     if len(wods_done) == num_wods:
-        """If all WODs are done, reset 
+        """If all WODs are done, reset
         the WODs_done file"""
-        open('static/bbdd/WODs_done.csv','w').close()
+        open(THIS_FOLDER / 'static/bbdd/WODs_done.csv','w').close()
 
     wod_number_not_done = random.randrange(num_wods)
-    
+
     while wod_number_not_done in wods_done:
         wod_number_not_done = random.randrange(num_wods)
 
@@ -87,10 +90,10 @@ def get_wod_number_not_done(num_wods, wods_done):
 
 
 def set_wod_done_in_file(wod_number_done):
-    with open('static/bbdd/WODs_done.csv', 'a') as file:
+    with open(THIS_FOLDER / 'static/bbdd/WODs_done.csv', 'a') as file:
         file.write(str(wod_number_done)+'\n')
 
-
+print(THIS_FOLDER)
 wods = get_all_wods()
 wods_done = get_wods_done()
 wod_number = get_wod_number_not_done(int(len(wods)), wods_done)
